fc0db592b2781691b06fa0d1a28d4aeb815256b2
eduardof5sg <149663359+eduardof5sg@users.noreply.github.com>
2024-05-10 15:31:00 +0200

    frontend_home001_add postService and homeComponent  to show all post's in homePage_

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,15 +1,52 @@
 import { Component } from '@angular/core';
 import { IonicModule } from '@ionic/angular';
+import { PostServiceService } from '../services/postService/post.service';
+import { OnInit } from '@angular/core';
+import { InfiniteScrollCustomEvent } from '@ionic/angular';
 
-
+// import { BrowserModule } from '@angular/platform-browser';
+import { CommonModule } from '@angular/common';
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
   standalone: true,
-  imports: [IonicModule],
+  imports: [IonicModule, CommonModule],
 })
-export class HomePage {
-  constructor() {}
+export class HomePage implements OnInit{
+
+  posts :any[]=[];
+  isImagetrue : boolean = false;
+  constructor(private postService: PostServiceService) {
+    
+  }
   menuType: string = 'overlay'
+
+  async ngOnInit(): Promise<void> {
+     this.generatePost()
+  }
+  
+  async  generatePost(){
+    try {
+     const response= await this.postService.getAllPost()
+     this.posts = response.data
+     
+     console.log(this.posts)
+        
+      } catch (error) {
+        console.error('Error to fetch Posts')
+      }
+  }
+
+  onIonInfinite(ev:any) {
+    this.generatePost();
+    setTimeout(() => {
+      (ev as InfiniteScrollCustomEvent).target.complete();
+    }, 500);
+  }
+  
+  isImage(file: string): boolean {
+    const isImage = /\.(gif|jpe?g|tiff?|png|webp|bmp)(\?.*)?$/i.test(file);
+    return isImage;
+  }  
 }
--- src/app/services/postService/post.service.ts
+++ src/app/services/postService/post.service.ts
@@ -1,16 +1,17 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
-import { Observable } from 'rxjs';
+// import { HttpClient } from '@angular/common/http';
+// import { Observable } from 'rxjs';
+import axios from 'axios';
 
 @Injectable({
   providedIn: 'root'
 })
 export class PostServiceService {
-  private apiUrl = 'http://localhost:3000/posts'
+  private apiUrl: string = 'http://localhost:3000/posts'
 
-  constructor(private http: HttpClient) { }
+  constructor() { }
 
-  CreatePost(newPost: any): Observable<any>{
+ async CreatePost(newPost: any){
     const PostData = {
       users_id: newPost.users_id,
       file: newPost.file,
@@ -18,23 +19,23 @@ export class PostServiceService {
       description: newPost.description,
       category: newPost.category
     }
-    return this.http.post(this.apiUrl, PostData)
+    return axios.post(this.apiUrl, PostData)
   }
 
-  getAllPost(): Observable<any> {
-    return this.http.get(this.apiUrl);
+ async getAllPost()  {
+    return axios.get(this.apiUrl);
   }
 
-  getPostById(posts_id: string): Observable<any> {
-    return this.http.get(`${this.apiUrl}/${posts_id}`);
-  }
+//   getPostById(posts_id: string): Observable<any> {
+//     return this.http.get(`${this.apiUrl}/${posts_id}`);
+//   }
 
-  updatePost(posts_id: string, updatePost: any): Observable<any> {
-    return this.http.put(`${this.apiUrl}/${posts_id}`, updatePost);
-  }
+//   updatePost(posts_id: string, updatePost: any): Observable<any> {
+//     return this.http.put(`${this.apiUrl}/${posts_id}`, updatePost);
+//   }
 
-  deletePost(posts_id: string): Observable<any> {
-    return this.http.delete(`${this.apiUrl}/${posts_id}`);
-  }
+//   deletePost(posts_id: string): Observable<any> {
+//     return this.http.delete(`${this.apiUrl}/${posts_id}`);
+//   }
 
 }
