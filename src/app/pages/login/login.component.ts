0a8128748a6241e6c3bc412765ff994c67993fac
jara.mayonesio@gmail.com <jara.mayonesio@gmail.com>
2024-05-13 10:56:03 +0200

    Frontend_login004_Login components and controllers in login screen

--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -1,13 +1,46 @@
 import { Component} from '@angular/core';
 import { IonicModule } from '@ionic/angular';
+import {FormGroup, FormControl, Validators, ReactiveFormsModule} from '@angular/forms';
+import { CommonModule } from '@angular/common';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.scss'],
   standalone: true,
-  imports: [IonicModule],
+  imports: [IonicModule, ReactiveFormsModule, CommonModule],
 })
-export class LoginComponent {
 
+export class LoginComponent {
+  //validacion email y contraseña
+  get email() {
+    return this.formUser.get('email') as FormControl;
+  } 
+  get password() {
+    return this.formUser.get('password') as FormControl;
   }
+  //controladores
+  formUser = new FormGroup ({
+    'email' : new FormControl('', [Validators.required, Validators.email]),
+    'password' : new FormControl('', [Validators.required])
+
+    areAllFIeldsFilled() : boolean{
+      const formValues = this.formUser.value as {[key:string]: string | null };
+      for ( const key in formValues) {
+        if (formValues.hasOwnProperty(key)){
+          const value: string | null = formValues[key];
+          if (!value) {
+            return false;
+          }
+        }
+        return true;
+      }
+    } 
+  })
+
+
+
+  
+}
+
