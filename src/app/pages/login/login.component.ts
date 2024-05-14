d3fe547b6385fd84c15a6c0fdf21f3b31f2ccbbd
jara.mayonesio@gmail.com <jara.mayonesio@gmail.com>
2024-05-14 09:53:58 +0200

    Frontend_login007_Login updated

--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -1,8 +1,8 @@
-import { Component} from '@angular/core';
+import { Component } from '@angular/core';
 import { IonicModule } from '@ionic/angular';
-import {FormGroup, FormControl, Validators, ReactiveFormsModule} from '@angular/forms';
+import { FormGroup, FormControl, Validators, ReactiveFormsModule } from '@angular/forms';
 import { CommonModule } from '@angular/common';
-import { UsersService } from '../../services/users.service'; 
+import { UsersService } from '../../services/users.service';
 import { Router } from '@angular/router';
 
 @Component({
@@ -17,31 +17,83 @@ export class LoginComponent {
   //validacion email y contraseña
   get email() {
     return this.formUser.get('email') as FormControl;
-  } 
+  }
   get password() {
     return this.formUser.get('password') as FormControl;
   }
   //controladores
-  formUser = new FormGroup ({
-    'email' : new FormControl('', [Validators.required, Validators.email]),
-    'password' : new FormControl('', [Validators.required])
+  formUser = new FormGroup({
+    'email': new FormControl('', [Validators.required, Validators.email]),
+    'password': new FormControl('', [Validators.required])
 
-    areAllFIeldsFilled() : boolean{
-      const formValues = this.formUser.value as {[key:string]: string | null };
-      for ( const key in formValues) {
-        if (formValues.hasOwnProperty(key)){
-          const value: string | null = formValues[key];
-          if (!value) {
-            return false;
-          }
-        }
-        return true;
-      }
-    } 
+    // areAllFieldsFilled(): boolean {
+    //   const formValues = this.formUser.value as { [key: string]: string | null };
+    //   for (const key in formValues) {
+    //     if (formValues.hasOwnProperty(key)) {
+    //       const value: string | null = formValues[key];
+    //       if (!value) {
+    //         return false;
+    //       }
+    //     }
+    //     return true;
+    //   }
+    // }
   })
 
+  //Constructor para las rutas de navegación de la página
 
+  constructor(private userService: UsersService, private router: Router) { }
 
-  
+  navigateToRegister() { //Ruta hacia Registro en el botón
+    this.router.navigate(["/register"])
+  }
+  navigateToHome() { //Ruta hacia Registro en el botón
+    this.router.navigate(["/home"])
+  }
+
+  showAlert = false;
+  alertMessage: string = '';
+  AlertMessage = false;
+
+  onSubmit() {
+    if (this.formUser.valid) {
+      const credentials = {
+        email: this.formUser.value.email,
+        password: this.formUser.value.password
+      }
+
+      this.userService.loginUser(credentials).subscribe(
+        (response) => {
+          console.log('login con éxito', response);
+          localStorage.setItem('Token de Usuario', response.tokenLog)
+          this.alertMessage = '¡Bienvenido, ' + credentials.email + ' !';
+          this.AlertMessage = true;
+          setTimeout(() => {
+            this.navigateToHome();
+          }, 2000);
+        },
+
+        (error) => {
+          console.error('Error al logear:', error);
+          this.alertMessage = 'Error en usuario/contraseña';
+          this.AlertMessage = true; // Mostrar la alerta
+          this.showAlert = true;
+
+          // Ocultar la alerta después de 3 segundos
+          setTimeout(() => {
+            this.AlertMessage = false;
+          }, 2000);
+        }
+      );
+
+    } else {
+      this.alertMessage = 'Por favor, complete todos los campos correctamente.';
+      this.AlertMessage = true; // Mostrar la alerta
+      this.showAlert = true;
+      setTimeout(() => {
+        this.AlertMessage = false;
+      }, 2000);
+    }
+
+  }
 }
-
