f74c8f55325f44fa82e1858bf840017a2fa94dce
jara.mayonesio@gmail.com <jara.mayonesio@gmail.com>
2024-05-21 12:11:12 +0200

    frontend_filter003_filter and component setted

--- src/app/pages/manage-post/manage-post.page.ts
+++ src/app/pages/manage-post/manage-post.page.ts
@@ -1,32 +1,37 @@
 import { Component, OnInit } from '@angular/core';
-import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { NavbarComponent } from 'src/app/components/navbar/navbar.component';
-// import { DetailUsersService } from 'src/app/services/detailUsers/detail-users.service';
 import { IonicModule } from '@ionic/angular';
-import { ListsComponent } from 'src/app/components/lists/lists.component';
+import { DetailUsersService } from 'src/app/services/detailUsers/detail-users.service';
+import { CommonModule } from '@angular/common';
+import { UserService } from 'src/app/services/userService/user.service';
 
 @Component({
   selector: 'app-manage-post',
   templateUrl: './manage-post.page.html',
   styleUrls: ['./manage-post.page.scss'],
-  standalone: true,
-  imports: [ CommonModule, FormsModule, NavbarComponent, IonicModule, ListsComponent]
+  imports: [ CommonModule, FormsModule, NavbarComponent, IonicModule],
+  standalone: true
 })
 export class ManagePostPage implements OnInit {
-  // details: any = {};
-  constructor() { }
+  details: any = {};
+  constructor(private DetailService: DetailUsersService, private userService: UserService) { }
 
   ngOnInit() {
-    // this.userAllDetails()
+    this.userAllDetails()
   }
-  // async userAllDetails() {
-  //   try {
-  //     const response = await this.DetailService.getAllDetails()
-  //     this.details = response.data;
-  //     console.log(response);
-  //   } catch (error) {
-      
-  //   }
-  // }
+  async userAllDetails() {
+    try {
+      const response = await this.DetailService.getAllDetails();
+      this.details = response.data;
+      console.log(response);
+
+      for (const detail of this.details) {
+        const userDetails = await this.userService.getUserById(detail.userId);
+        detail.userDetails = userDetails;
+      }
+    } catch (error) {
+      console.error(error);
+    }
+  }
 }
