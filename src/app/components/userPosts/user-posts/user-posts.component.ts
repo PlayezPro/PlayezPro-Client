9cced68760d63f69bd5b2071717a66d5c1de7f6d
eduardof5sg <149663359+eduardof5sg@users.noreply.github.com>
2024-05-28 09:37:07 +0200

    frontend_follows_was created button to add relation between users

---
+++ src/app/components/userPosts/user-posts/user-posts.component.ts
@@ -0,0 +1,38 @@
+import { CommonModule } from '@angular/common';
+import { Component, OnInit,Input, OnChanges ,SimpleChanges} from '@angular/core';
+import { IonicModule} from '@ionic/angular';
+import { PostServiceService } from 'src/app/services/postService/post.service';
+
+@Component({
+  selector: 'app-user-posts',
+  templateUrl: './user-posts.component.html',
+  styleUrls: ['./user-posts.component.scss'],
+  standalone: true,
+  imports: [CommonModule, IonicModule]
+})
+export class UserPostsComponent  implements OnInit, OnChanges {
+  @Input() users_id: string | null = null;
+  userId: string | null = null;
+  posts: any[] = []
+  constructor(private postService:PostServiceService) { }
+
+  ngOnInit() {
+    this.showUserPosts()
+  }
+
+  ngOnChanges(changes: SimpleChanges) {
+    if (changes['users_id'] && changes['users_id'].currentValue) {
+      this.showUserPosts();
+    }
+  }
+
+  async showUserPosts(){
+    try {
+      const response = await this.postService.getUserPosts(this.users_id);
+      this.posts = response.data
+      console.log(this.posts)
+    } catch (error) {
+      console.error('Error al mostrar los blogs del usuario', error);
+    }
+  }
+}
--- src/app/pages/manage-user/manage-user.page.ts
+++ src/app/pages/manage-user/manage-user.page.ts
@@ -6,13 +6,14 @@ import { NavbarComponent } from 'src/app/components/navbar/navbar.component';
 import { ActivatedRoute } from '@angular/router';
 import { FutcardComponent } from 'src/app/components/futcard/futcard.component';
 import { SkillsGraphicsComponent } from 'src/app/components/graphics/skills-graphics/skills-graphics.component';
+import { UserPostsComponent } from 'src/app/components/userPosts/user-posts/user-posts.component';
 
 @Component({
   selector: 'app-manage-user',
   templateUrl: './manage-user.page.html',
   styleUrls: ['./manage-user.page.scss'],
   standalone: true,
-  imports: [IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule, NavbarComponent,FutcardComponent,SkillsGraphicsComponent]
+  imports: [IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule, NavbarComponent,FutcardComponent,SkillsGraphicsComponent,UserPostsComponent]
 })
 export class ManageUserPage implements OnInit {
   users_id: string | null = null;
