5857dd1891614671f41f4d1a24b0fc9c5e702d81
jara.mayonesio@gmail.com <jara.mayonesio@gmail.com>
2024-06-03 10:21:44 +0200

    frontend_register15_changing loading and register styles

--- src/app/components/loader/loader.component.ts
+++ src/app/components/loader/loader.component.ts
@@ -1,10 +1,11 @@
 import { Component, OnInit } from '@angular/core';
+import { IonContent } from "@ionic/angular/standalone";
 
 @Component({
   selector: 'app-loader',
   templateUrl: './loader.component.html',
   styleUrls: ['./loader.component.scss'],
-  imports:[],
+  imports:[IonContent],
   standalone: true
 })
 export class LoaderComponent  implements OnInit {
